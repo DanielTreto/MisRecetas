f46e6314accddac95e988a4d0fdb15fd88b87d79
Daniel <tretoleondaniel@gmail.com>
2025-11-07 19:06:53 +0100

    Validación formulario

--- src/app/formulario/formulario.ts
+++ src/app/formulario/formulario.ts
@@ -1,41 +1,46 @@
 import { Component, output } from '@angular/core';
 import { Receta } from '../receta/receta';
-import { FormControl, FormGroup , Validators, ReactiveFormsModule} from '@angular/forms';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { recetaModel } from '../model/RecetaModel';
 
 @Component({
   selector: 'app-formulario',
   imports: [ReactiveFormsModule],
   templateUrl: './formulario.html',
-  styleUrl: './formulario.scss'
+  styleUrl: './formulario.scss',
 })
 export class Formulario {
-
   recetaNueva = output<recetaModel>();
 
   recetaForm = new FormGroup({
     titulo: new FormControl('', Validators.required),
     imagen: new FormControl('', Validators.required),
-    ingredientes: new FormControl('', Validators.required)
+    ingredientes: new FormControl('', Validators.required),
   });
 
-
-  get titulo(){
+  get titulo() {
     return this.recetaForm.get('titulo');
   }
 
-  get imagen(){
+  get imagen() {
     return this.recetaForm.get('imagen');
   }
 
-  get ingredientes(){
+  get ingredientes() {
     return this.recetaForm.get('ingredientes');
   }
 
-
-  submit(){
-    let receta = new recetaModel(this.recetaForm.value.titulo ?? '',this.recetaForm.value.imagen ?? '',this.recetaForm.value.ingredientes ?? '');
-    this.recetaNueva.emit(receta);
-    this.recetaForm.reset();
+  submit() {
+    if (this.recetaForm.valid) {
+      let receta = new recetaModel(
+        this.recetaForm.value.titulo ?? '',
+        this.recetaForm.value.imagen ?? '',
+        this.recetaForm.value.ingredientes ?? ''
+      );
+      this.recetaNueva.emit(receta);
+      this.recetaForm.reset();
+    } else {
+      alert('Formulario inválido. Revise los campos.');
+    }
   }
 }
